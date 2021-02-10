6324d330278f502c7c74ec74836c482b9abd2ddd
Brodie Peberdy <brodie.peberdy@student.manchester.ac.uk>
2021-02-10 16:54:56 +0000

    Amended some stuff in the base template, added a login form (non-functioning yet)

--- backend/frontend/static/js/login.js
+++ backend/frontend/static/js/login.js
@@ -3,7 +3,35 @@ var app = new Vue({
     data: {
         message : "Hello world!",
         left : false,
-        right : false
+        name: null,
+        password: '',
+        accept: false
     },
     delimiters: ['[%', '%]'],
+    methods: {
+    onSubmit () {
+      if (this.accept !== true) {
+        this.$q.notify({
+          color: 'red-5',
+          textColor: 'white',
+          icon: 'warning',
+          message: 'You need to accept the license and terms first'
+        })
+      }
+      else {
+        this.$q.notify({
+          color: 'green-4',
+          textColor: 'white',
+          icon: 'cloud_done',
+          message: 'Submitted'
+        })
+      }
+    },
+
+    onReset () {
+      this.name = null
+      this.age = null
+      this.accept = false
+    }
+  }
   });
