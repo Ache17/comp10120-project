38ef2fc96e5b0d884d224679cfb25040b9cc57b6
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2021-02-12 16:17:59 +0100

    basic REST

--- backend/REST/views.py
+++ backend/REST/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import serializers, status
 
 # Create your views here.
+class testView(APIView):
+    def get(self, request):
+        return Response("just a test")
+
+    def post(self, request):
+        return  Response("post request")
