b3439da89fb2d7170032eb24f818c08f6328d79d
Brodie Peberdy <brodie.peberdy@student.manchester.ac.uk>
2021-02-20 12:53:25 +0000

    Playlist creation form done, needs communciation with backend

--- backend/frontend/static/js/index.js
+++ backend/frontend/static/js/index.js
@@ -91,9 +91,64 @@ var app = new Vue({
 
             // do XHR
         },
-        submitPlaylist(msg)
+        // This is performed upon the user clicking the Submit Playlist button.
+        submitPlaylist()
         {
-            this.sucessNotification("Function was called.");
+          var NumberOfTracks = this.number_of_tracks;
+          var Tracks = [];
+          var Track = {};
+          for (var i = 1; i <= NumberOfTracks; i++)
+          {
+            Track = {Title:document.getElementById("TitleInput" + i).value, Artist:document.getElementById("ArtistInput" + i).value, Year:document.getElementById("YearInput" + i).value, Rating:document.getElementById("YearInput" + i).value}
+            Tracks.push(Track)
+          }
+          data =
+          {
+            PlaylistTitle: this.playlist_title,
+          	PlaylistGenre: this.playlist_genre,
+          	// ImageURL: ImageURL,
+          	Description: this.playlist_description,
+            Public: this.public,
+            Tracks: Tracks
+          };
+          console.log(data);
         }
     }
   });
+
+// Function is called whenever the user changes the counter for the number of tracks to be in the playlist, and essentially generates the suitable number of inputs that is required.
+function TrackNumberChange()
+{
+  var number_of_tracks = app.number_of_tracks;
+  // Cycles through the number of tracks determined by the user. If they have increased the number of tracks, then there will be track inputs that need to be created, and so performs this when necessary.
+  for (var i = 1; i <= number_of_tracks; i++)
+  {
+    var element = document.getElementById("TitleInput" + i);
+    if(!element)
+    {
+      CreateTrackInput(i);
+    }
+  }
+  // If the user decreases the counter, then the no longer required track inputs can be removed from the document, using jQuery.
+  for (var i = number_of_tracks + 1; i <= 50; i++)
+  {
+    $("#TitleInput" + i).remove();
+    $("#ArtistInput" + i).remove();
+    $("#YearInput" + i).remove();
+    $("#RatingInput" + i).remove();
+    $("#Break" + i).remove();
+  }
+}
+
+// Takes a track number, and uses jQuery to dynamically append HTML elements to the document which can be identified by their track number.
+function CreateTrackInput(i)
+{
+  // Elements + attributes etc. for the appending of track inputs.
+  TitleInput = $("<input>").attr("id", "TitleInput" + i).attr("placeholder", "Track Title").attr("size", "40");
+  ArtistInput = $("<input>").attr("id", "ArtistInput" + i).attr("placeholder", "Artist Name").attr("size", "30"); // The value of the artist name can be automatically populated as the user has already entered this during the album's creation, although can be altered if desired.
+  YearInput = $("<input>").attr("id", "YearInput" + i).attr("type", "number").attr("min", "1860").attr("max", "2030").attr("placeholder", "Year");
+  RatingInput = $("<input>").attr("id", "RatingInput" + i).attr("type", "number").attr("min", "1").attr("max", "5").attr("placeholder", "Rating").attr("style", "width: 7em");;
+  BreakLine = $("<br><br>").attr("id", "Break" + i);
+  // Appends all of these elements to the HTML document.
+  $("#TrackInputs").append(TitleInput, ArtistInput, YearInput, RatingInput, BreakLine);
+}
