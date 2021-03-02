e9d4951428aa9a3a037c617b33720431a2fbba86
Maciej Lewandowski <lewandowski.maciej1702@gmail.com>
2021-03-02 10:28:55 +0100

    authentication

--- backend/REST/views.py
+++ backend/REST/views.py
@@ -9,9 +9,9 @@ from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import User
 from .serializers import UserSerializer
 from rest_framework import status
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated, AllowAny
+
+
 # Create your views here.
 class testView(APIView):
     def get(self, request):
@@ -20,7 +20,17 @@ class testView(APIView):
     def post(self, request):
         return  Response("post request")
 
+
+class authenticatedTest(APIView):
+    def get(self, request):
+        return Response("get  success !")
+
+    def post(self, request):
+        return Response("post success !")
+
+
 class registerView(APIView):
+    permission_classes = [AllowAny]
     def post(self, request):    
         print(request.data)
         
@@ -30,35 +40,3 @@ class registerView(APIView):
             return Response({"message" : "registation sucessfull"}, status=status.HTTP_201_CREATED)
         return Response({"message" : "registration not sucessfull"}, status=status.HTTP_400_BAD_REQUEST)
 
-
-class ListUsers(APIView):
-    """
-    View to list all users in the system.
-
-    * Requires token authentication.
-    * Only admin users are able to access this view.
-    """
-    authentication_classes = [authentication.TokenAuthentication]
-    permission_classes = [permissions.IsAuthenticated]
-
-    def get(self, request, format=None):
-        """
-        Return a list of all users.
-        """
-        usernames = [user.username for user in User.objects.all()]
-        return Response(usernames)
-
-
-class CustomAuthToken(ObtainAuthToken):
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data,
-                                           context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'user_id': user.pk,
-            'email': user.email
-        })
