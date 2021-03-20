33cf2c23df32616816669560fdcda251c388bcf7
Yi Gao <yi.gao-2@student.manchester.ac.uk>
2021-03-20 12:08:48 +0000

    retreves

--- backend/REST/views.py
+++ backend/REST/views.py
@@ -435,17 +435,20 @@ class spotifyQuery(APIView):
 
 class retreves(APIView):
 
-    permission_classes = [AllowAny]
+    permission_classes = (IsAuthenticated,)
 
     def get(self, request):
-
+    
+        if "userid" in request.data:
+            UserID = request.data["userid"]
         try:
             theUser = Playlist.objects.get(link=UserID)
-        except User.notexist:
+        except User.DoseNotExists:
             return Response({"message": "user not found!"}, status=status.HTTP_400_BAD_REQUEST)
 
         username = theUser.username
         group = theUser.group
         last_login = theUser.last_login
         date_joined = theUser.date_joined
+        return Response({"username" : theUser.username}, status=status.HTTP_200_OK)
 
